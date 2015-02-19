326d01d6291e79e2ca0ee57875131c22860541c0
bajormar <marius.bajorunas@gmail.com>
2015-02-19 21:45:04 +0200

    Created angular filters branch

--- amazing.js
+++
@@ -1,7 +0,0 @@
-angular.module('app').filter('amazing', function amazingFilterDefinition() {
-  return function amazingFilter(input) {
-    return (input || '').replace(
-      /lame|boring|dull|pointless|slow/ig, 'AMAZING'
-    );
-  };
-});
