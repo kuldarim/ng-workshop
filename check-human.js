895a94944eaf1f55a23368d9c13952512c9a2609
Rimvydas Kulda <rimvydas.kulda@tieto.com>
2015-02-20 16:51:06 +0200

    fixed stuff

--- check-human.js
+++ check-human.js
@@ -1,34 +1,34 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('app').directive('checkHuman', checkHuman);
+    angular.module('app').directive('checkHuman', checkHuman);
 
-  function checkHuman() {
-    return {
-      restrict: 'E',
-      templateUrl: 'check-human.html',
-      scope: {
-        valid: '=isHuman'
-      },
-      controllerAs: 'vm',
-      bindToController: true,
-      controller: function checkHumanCtrl() {
-        var vm = this;
+    function checkHuman() {
+        return {
+            restrict: 'E',
+            templateUrl: 'check-human.html',
+            scope: {
+                valid: '=isHuman'
+            },
+            controllerAs: 'vm',
+            bindToController: true,
+            controller: function checkHumanCtrl() {
+                var vm = this;
 
-        // variable assignment
-        vm.valid = false;
-        vm.rand1 = Math.floor(Math.random() * 5) + 1;
-        vm.rand2 = Math.floor(Math.random() * 5) + 1;
-        var correct = vm.rand1 + vm.rand2;
+                // variable assignment
+                vm.valid = false;
+                vm.rand1 = Math.floor(Math.random() * 5) + 1;
+                vm.rand2 = Math.floor(Math.random() * 5) + 1;
+                var correct = vm.rand1 + vm.rand2;
 
-        // function assignment
-        vm.check = check;
+                // function assignment
+                vm.check = check;
 
-        // function declaration
-        function check() {
-          vm.valid = parseInt(vm.answer, 10) === correct;
-        }
-      }
-    };
-  }
+                // function declaration
+                function check() {
+                  vm.valid = parseInt(vm.answer, 10) === correct;
+                }
+            }
+        };
+    }
 })();
